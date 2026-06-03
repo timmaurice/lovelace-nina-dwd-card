25016196cc72b264a6435bd79fcc64f1678bde63
Tim Maurice Bayer <mail@timbayer.de>
2026-06-03 16:49:13 +0200

    chore: update dependencies, refine configuration, and bump version to 1.3.4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
-    ignores: ['dist/'],
+    ignores: ['dist/', 'test-env/'],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
